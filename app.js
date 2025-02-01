4b89446fed8dfd1f9f55ad6d81a221ceb6881428
wilwilpopcorn <wilwilpopcorn@outlook.com>
2025-02-01 11:59:55 -0300

    Adicionando a  Function SORTEAR

---
+++ app.js
@@ -0,0 +1,8 @@
+function sortear(){
+    let  quantidade = document.getElementById('quantidade').value; //Recupera o elemento com o ID e o valor no input
+    let  de = document.getElementById('de').value; 
+    let  ate = document.getElementById('ate').value; 
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
+}
